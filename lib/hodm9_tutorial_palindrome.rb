fb4bc72a2e1310916aab5d3788227b256a783c31
Applepie9 <horel.dora@gmail.com>
2021-04-22 12:17:30 +0200

    Initialise repository

---
+++ lib/hodm9_tutorial_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hodm9_tutorial_palindrome/version"
+
+module Hodm9TutorialPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "hodm9_tutorial_palindrome"
+
+require "minitest/autorun"
